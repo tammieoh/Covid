03db613746a24e3fc779f297f4cdfb0e373480d3
tammieoh <oht@oxy.edu>
2020-10-20 16:08:09 -0700

    Update Country.js

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -12,8 +12,8 @@ class Country extends Component {
         this._buttonClicked = this._buttonClicked.bind(this);
         this.state = {
             countries: [],
-            countryStart = "",
-            countryEnd = ""
+            countryStart: "",
+            countryEnd: ""
         }
     }
     componentDidMount() {
@@ -21,7 +21,6 @@ class Country extends Component {
         //  countryName is going to be taken from the url (the routing link)
         // then we're going to replace it in the link
         const {countryName} = this.props.match.params;
-        console.log(countryName);
         // console.log(`https://api.covid19api.com/country/${countryName}?from=2020-10-01T00:00:00Z&to=2020-10-15T00:00:00Z`);
         
         // i'm using this API call to fetch the information about confirmed, recovered cases and deaths
@@ -29,13 +28,12 @@ class Country extends Component {
         fetch(`https://api.covid19api.com/country/${countryName}?from=2020-10-01T00:00:00Z&to=2020-10-15T00:00:00Z`)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
             this.setState({
                 //  this is an array because the parameters are dates
                 // Oct 1 - Oct 15 so there's going to be multiple array elements
                 countries: data,
-                countryStart: countries[0],
-                countryEnd: countries[data.length - 1]
+                countryStart: data[0],
+                countryEnd: data[data.length - 1]
             })
         })
     }
@@ -46,45 +44,12 @@ class Country extends Component {
     }
     render(){
         // const {country} = this.props.match.params;
-        let info = 0;
-        const total = this.state.countries.map((country) => 
-                info = [country.Confirmed, country.Recovered, country.Deaths]
-                )
-        console.log(info);
-
         return(
             <div className="country">
-                {/* <div>
-                    {this.state.countries.map((country) => <p key={country.Date} value={country.Date}>Date: {country.Date}  <br></br> Confirmed Cases: {country.Confirmed}  <br></br> Recovered Cases: {country.Recovered}  <br></br> Deaths: {country.Deaths}</p>)}
-                </div> 
-                */}
-
-                <div className="flip-card">
-                    <div className="flip-card-inner">
-                        <div className="flip-card-front">
-                            <p>hello</p>
-                        </div>
-                        <div className="flip-card-back">
-                            <p>goodbye</p>
-
-                        </div>
-                    </div>
+                <div>
+                    {/*  this shows information about the cases and deaths*/}
+                    <p key={this.state.countryEnd.Date} value={this.state.countryEnd.Date}>Date: {this.state.countryEnd.Date}  <br></br> Confirmed Cases: {this.state.countryEnd.Confirmed - this.state.countryStart.Confirmed} </p>
                 </div>
-
-                <div className="flip-card">
-                    <div className="flip-card-inner">
-                        <div className="flip-card-front">
-                            <p>hello</p>
-                        </div>
-                        <div className="flip-card-back">
-                            <p>goodbye</p>
-
-                        </div>
-                    </div>
-                </div>
-  
-
-
                  <div className="goBack">
                     <button onClick={this._buttonClicked}>Go Back</button>
                  </div>
